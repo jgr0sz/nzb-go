4c7cbb437ec82355cd3b0df57ca099e9e3c7cbcf
jgr0sz <243434564+jgr0sz@users.noreply.github.com>
2025-12-26 18:22:22 -0500

    Update models.go
    Updated comment

--- metaeditor/models.go
+++ metaeditor/models.go
@@ -2,7 +2,7 @@ package metaeditor
 
 import "jgr0sz/nzbgo/parser"
 
-//Main object struct for the meta editor. Contains a Meta splice to hold attributes and their content.
+//Main object struct for the meta editor. Contains a Meta splice to hold attributes and their content and a pointer to the original Nzb
 type NzbMetaEditor struct {
 	Metadata []parser.Meta
 	Nzb *parser.Nzb
